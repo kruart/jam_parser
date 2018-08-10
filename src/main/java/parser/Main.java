40ec2ccdcd78612292f42988ffb6c4cb32df6f38
we0z <weoz@ukr.net>
2018-08-10 11:15:02 +0300

    initial java project

---
+++ src/main/java/parser/Main.java
@@ -0,0 +1,9 @@
+package parser;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(String.join("----", Arrays.asList("Alice", "Eva", "Bob")));
+    }
+}
