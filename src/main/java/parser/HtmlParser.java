b8053052da4792f7d7a8af92858db88b4900cd6e
we0z <weoz@ukr.net>
2018-08-10 18:21:45 +0300

    jam-parser java realization

---
+++ src/main/java/parser/HtmlParser.java
@@ -0,0 +1,102 @@
+package parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class HtmlParser {
+
+    public static String getHtml(String url) throws IOException {
+        return Jsoup.connect(url).get().html();
+    }
+
+    public static List<String> getLinksFromDiv(String html, String elementId) {
+        Document doc = Jsoup.parse(html);
+        Elements divs = doc.select("div#" + elementId);
+        return divs.stream()
+                .map(div -> div.getElementsByTag("a").attr("href"))
+                .collect(Collectors.toList());
+    }
+
+    public static String getNumOfPages(String html) {
+        Document doc = Jsoup.parse(html);
+        String href = doc.select("div#catalog_pager")
+                .last()
+                .getElementsByTag("a")
+                .last()
+                .attr("href");
+
+        return href.substring(href.lastIndexOf("=") + 1);
+    }
+
+    public static Map<String, List<String>> getImageLinks(String html) {
+        List<String> imgLinks = new ArrayList<>();
+        String domain = "https://jam.ua";
+
+        Document doc = Jsoup.parse(html);
+        String title = getTextFromElement(doc, "div#item_header_name > h1");
+        if (!doc.select("div.img-item-gallery").isEmpty()) {
+            imgLinks.addAll(getImgLinksFromGallery(doc, domain));
+        } else {
+            imgLinks.add(getMainImageLink(doc, domain));
+        }
+
+//        return Map.of(title, imgLinks); //java 9
+        return Collections.unmodifiableMap(new HashMap<String, List<String>>() {{ put(title, imgLinks);}});
+    }
+
+    public static String getTextFromElement(Document doc, String element) {
+        return doc.selectFirst(element).text();
+    }
+
+    private static String getMainImageLink(Document doc, String domain) {
+        String beginningImageLink = "/files/";
+        Element parentDiv = doc.selectFirst("div#item_image_wrapper");
+        String itemImg = parentDiv.select("div#item_img > a > div").attr("style");
+        int startIndex = itemImg.indexOf(beginningImageLink);
+        String link = itemImg.split("'\\)")[0];
+
+        return domain + itemImg.substring(startIndex, link.length());   //full link
+    }
+
+    private static List<String> getImgLinksFromGallery(Document doc, String domain) {
+        return doc.select("div.img-item-block > a").stream()
+                .map(a -> domain + a.attr("href"))
+                .collect(Collectors.toList());
+    }
+
+    public static void downloadImages(Map<String, List<String>> data) throws IOException {
+        String parentFolder = "./images/";
+
+        for (Map.Entry<String, List<String>> entry : data.entrySet()) {
+            for (String link : entry.getValue()) {
+                System.out.println("start working on " + entry.getKey() + " " + link);
+                Path path = Paths.get(parentFolder + entry.getKey());
+
+
+                if (!Files.exists(path)) {
+                    Files.createDirectories(path);
+                }
+
+                String fullPath = path.toString() + File.separator + getImgName(link);
+
+                try(InputStream in = new URL(link).openStream()){
+                    Files.copy(in, Paths.get(fullPath), StandardCopyOption.REPLACE_EXISTING);
+                }
+            }
+        }
+    }
+
+    private static String getImgName(String link) {
+        return link.substring(link.lastIndexOf("/") + 1);
+    }
+}
--- src/main/java/parser/Main.java
+++ src/main/java/parser/Main.java
@@ -1,9 +1,51 @@
 package parser;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
+    private static int currentPage = 1;
+    private static int numOfPages = 1;
+
     public static void main(String[] args) {
-        System.out.println(String.join("----", Arrays.asList("Alice", "Eva", "Bob")));
+        List<String> links = new ArrayList<>();
+
+        try {
+            while (currentPage <= numOfPages) {
+                String url = String.format("https://jam.ua/picks?list=%d", currentPage);
+                String html = HtmlParser.getHtml(url);
+                links.addAll(HtmlParser.getLinksFromDiv(html, "catalog_item"));
+
+                if (numOfPages == 1) {
+                    numOfPages = Integer.parseInt(HtmlParser.getNumOfPages(html));
+                }
+
+                currentPage++;
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong...");
+        }
+        links.parallelStream().forEach(link -> {
+            try {
+                String html = HtmlParser.getHtml(link);
+                Map<String, List<String>> imgLinks = HtmlParser.getImageLinks(html); //<title, img links>
+                HtmlParser.downloadImages(imgLinks);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+//        String html = null;
+//        try {
+//            html = HtmlParser.getHtml("https://jam.ua/martin-picks-18a0050-1pick-pack-073");
+//            Map<String, List<String>> imgLinks = HtmlParser.getImageLinks(html);
+//            HtmlParser.downloadImages(imgLinks);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
     }
 }
